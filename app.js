9981af9efdc34a8989eaae993f7bbb05d64c1bda
Godwin <rualgodwin@gmail.com>
2022-10-06 20:59:29 +0800

    added comment and post pop-up incomplete

---
+++ app.js
@@ -0,0 +1,13 @@
+let popUp = document.getElementsByClassName("pstp-container");
+
+function openPost() {
+  popup.classList.add("open-pstp");
+}
+
+function closePost() {
+  popup.classList.remove("open-pstp");
+}
+
+function togglePopup() {
+  document.getElementById("pop-up-1").classList.toggle("active");
+}
